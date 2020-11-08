1bdc4d478633ee6fdbe17c0e75732b83a89bfbe2
hsi05200 <65710972+hsi05200@users.noreply.github.com>
2020-11-09 00:09:43 +0900

    App.js React 초기작업 준비상태로 설정

--- src/App.js
+++ src/App.js
@@ -4,60 +4,14 @@ import Toc from "./components/Toc";
 import Content from "./components/Content";
 import { Component } from 'react';
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      mode:'read',
-      selected_content_id:2,
-      subject:{title:'WEB', sub:'World Wid Web!'},
-      welcome:{title:'Welcome', desc:'hello, React!!'},
-      contents:[
-        {id:1, title:'HTML', desc:'HTML is for information'},
-        {id:2, title:'CSS', desc:'CSS is for design'},
-        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
-      ]
-    }
-  }
-  render() {
-    var _title, _desc = null;
-    if(this.state.mode === 'welcome'){
-      _title = this.state.welcome.title;
-      _desc = this.state.welcome.desc;
-    } else if(this.state.mode === 'read'){
-      var i = 0;
-      while(i < this.state.contents.length){
-        var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id){
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i = i + 1;
-      }      
-    }   
+class App extends Component {  
+  render() {    
     return (
       <div className="App">       
-        <Subject
-          title={this.state.subject.title}
-          sub={this.state.subject.sub}
-          onChangePage={function(){            
-            this.setState({mode:'welcome'});
-          }.bind(this)}
-        >
-        </Subject>        
-        <Toc 
-          onChangePage={function(id){
-            this.setState({
-              mode:'read',
-              selected_content_id:Number(id)
-            });
-          }.bind(this)}
-          data={this.state.contents}
-        ></Toc>
-        <Content title={_title} desc={_desc}></Content>
+        Hello, React!!        
       </div>      
     );
   }
 }
+
 export default App;
