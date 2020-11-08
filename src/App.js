aade05936f604d93bd62a0919382d791ca316a84
hsi05200 <65710972+hsi05200@users.noreply.github.com>
2020-11-09 00:21:23 +0900

    콤포넌트(component)를 만들기 위한 작업준비 - 기존 component 3개파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import './App.css';
-import Subject from "./components/Subject";
-import Toc from "./components/Toc";
-import Content from "./components/Content";
 import { Component } from 'react';
+import './App.css';
 
 class App extends Component {  
   render() {    
