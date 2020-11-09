88975e6fc29b4e6a03212ee8a0ef7071d13212b9
hsi05200 <65710972+hsi05200@users.noreply.github.com>
2020-11-09 21:13:11 +0900

    외부에 보여질 props 값을 state 방식을 통해서 전달하기위해 props를 초기화하고 state를 생성하여 외부의 porps값으로 전달하는 state 코드생성

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import Content from "./components/Content";
 import Subject from "./components/Subject";
 import './App.css';
 
-class App extends Component {  
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!'}
+    }
+  }
   render() {    
     return (
       <div className="App">       
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        {/* <Subject title="REACT" sub="For UI"></Subject> */}
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}>
+        </Subject>        
         <Toc></Toc>
         <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
       </div>      
