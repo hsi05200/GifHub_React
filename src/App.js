1266b113f5c9c3ce1318b25b0ec33e281afabc2f
hsi05200 <65710972+hsi05200@users.noreply.github.com>
2020-11-09 22:16:26 +0900

    1. state를 생성하기 위해 constructor(props) {  super(props); }값을 초기화하는 작업 2. state값을 배열로 생성하고 내부 component Toc에서 data라는 값으로 주입함 3. Toc.js내에 목록을 반복적으로 생성하기 위해 반복문 while를 사용하여 lists[ ]라는 배열을 통해 목록을 생성

--- src/App.js
+++ src/App.js
@@ -8,7 +8,13 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      subject:{title:'WEB', sub:'World Wide Web!'}
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
+        {id:4, title:'React', desc:'React is for component'}
+      ]
     }
   }
   render() {    
@@ -18,7 +24,7 @@ class App extends Component {
           title={this.state.subject.title}
           sub={this.state.subject.sub}>
         </Subject>        
-        <Toc></Toc>
+        <Toc data={this.state.contents}></Toc>
         <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
       </div>      
     );
