b5bf89e704a9558979b5523766fa62ec83e5de6b
hsi05200 <65710972+hsi05200@users.noreply.github.com>
2020-11-11 22:23:53 +0900

    호출태그내 onchangePage값을 등록하고 이것을 onClick으로 호출하여 클릭이벤트를 발생시킴. function값으로는 this.state.mode 방식의 호출이 아닌 this.setState({모드값}) 으로 호출하여야 한다.

--- src/App.js
+++ src/App.js
@@ -31,20 +31,16 @@ class App extends Component {
     }
     return (
       <div className="App">       
-        {/* <Subject
+        <Subject
           title={this.state.subject.title}
-          sub={this.state.subject.sub}>
-        </Subject> */}
-        <header>
-          <h1><a href="./" onClick={function(e){            
-            e.preventDefault();
-            //this.state.mode = 'welcome';
+          sub={this.state.subject.sub}
+          onChangePage={function(){
             this.setState({
               mode:'welcome'
-            });
-          }.bind(this)}>{this.state.subject.title}</a></h1>
-          {this.state.subject.sub}
-        </header>  
+            })
+          }.bind(this)}
+        >          
+        </Subject>        
         <Toc data={this.state.contents}></Toc>
         <Content title={_title} desc={_desc}></Content> 
       </div>      
