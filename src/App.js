28883fc5b3480f724e622798d45c6b892586ba08
stchwang <hsi05200@gmail.com>
2020-11-12 23:27:52 +0900

    Control compotent를 추가하여 onClick 이벤트를 통해 mode를 변경작업

--- src/App.js
+++ src/App.js
@@ -2,65 +2,69 @@ import { Component } from 'react';
 import Toc from "./components/Toc";
 import Content from "./components/Content";
 import Subject from "./components/Subject";
+import Control from "./components/Control";
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode:'read',
-      selected_content_id:2,
-      subject:{title:'WEB', sub:'World Wide Web!'},
-      welcome:{title:'Welcome', desc:'Hello, React!!'},
-      contents:[
-        {id:1, title:'HTML', desc:'HTML is for information'},
-        {id:2, title:'CSS', desc:'CSS is for design'},
-        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
-        {id:4, title:'React', desc:'React is for component'},
-        {id:5, title:'PHP', desc:'PHP is for programing'},
-        {id:6, title:'NodeJs', desc:'NodeJs is for programing'}
+      mode: 'read',
+      selected_content_id: 2,
+      subject: { title: 'WEB', sub: 'World Wide Web!' },
+      welcome: { title: 'Welcome', desc: 'Hello, React!!' },
+      contents: [
+        { id: 1, title: 'HTML', desc: 'HTML is for information' },
+        { id: 2, title: 'CSS', desc: 'CSS is for design' },
+        { id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive' }
       ]
     }
   }
-  render() { 
+  render() {
     var _title, _desc = null;
-    if(this.state.mode === 'welcome'){
+    if (this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
-    } else if(this.state.mode === 'read') {
+    } else if (this.state.mode === 'read') {
       var i = 0;
-      while(i < this.state.contents.length) {
+      while (i < this.state.contents.length) {
         var date = this.state.contents[i];
-        if(date.id === this.state.selected_content_id) {
+        if (date.id === this.state.selected_content_id) {
           _title = date.title;
           _desc = date.desc;
           break;
         }
         i = i + 1;
-      }      
+      }
     }
     return (
-      <div className="App">       
+      <div className="App">
         <Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}
-          onChangePage={function(){
+          onChangePage={function () {
             this.setState({
-              mode:'welcome' 
+              mode: 'welcome'
             })
           }.bind(this)}
-        >          
-        </Subject>        
-        <Toc 
-          onChangePage={function(id){
+        >
+        </Subject>
+        <Toc
+          onChangePage={function (id) {
             this.setState({
-              mode:'read',
-              selected_content_id:Number(id)
+              mode: 'read',
+              selected_content_id: Number(id)
             });
           }.bind(this)}
-          data={this.state.contents}></Toc>
-        <Content title={_title} desc={_desc}></Content> 
-      </div>      
+          data={this.state.contents}>            
+        </Toc>
+        <Control onChangeMode={function(_mode){
+          this.setState({
+            mode:_mode
+          });
+        }.bind(this)}></Control>
+        <Content title={_title} desc={_desc}></Content>
+      </div>
     );
   }
 }
---
+++ src/components/Control.js
@@ -0,0 +1,24 @@
+import { Component } from 'react';
+
+class Control extends Component {  
+  render() {    
+    return (
+      <ul>
+        <li><a href="/create" onClick={function(e){
+          e.preventDefault();
+          this.props.onChangeMode('create');
+        }.bind(this)}>추가</a></li>
+        <li><a href="/update" onClick={function(e){
+          e.preventDefault();
+          this.props.onChangeMode('update');
+        }.bind(this)}>수정</a></li>
+        <li><input onClick={function(e){
+          e.preventDefault();
+          this.props.onChangeMode('delete');
+        }.bind(this)} type="button" value="삭제" /></li>
+      </ul>   
+    );
+  }
+}
+
+export default Control;
