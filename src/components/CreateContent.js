3f5d4cfc7b6c8f0b30c88cd86214bd36527d8bdb
stchwang <hsi05200@gmail.com>
2020-11-16 22:01:49 +0900

    CreateContent.js에 입력폼 추가작업후 이벤트발생 체크

--- src/components/CreateContent.js
+++ src/components/CreateContent.js
@@ -5,8 +5,21 @@ class CreateContent extends Component {
     return (
       <article>
         <h2>추가</h2>
-        <form>
-      
+        <form action="/create_process" method="post"
+          onSubmit={function(e){
+            e.preventDefault();
+            alert('Submit!!');
+          }.bind(this)}
+        >
+          <p>
+            <input type="text" name="title" placeholder="제목입력" style={{width:'342px'}}/>
+          </p>
+          <p>
+            <textarea cols="50" rows="5" type="text" name="desc" placeholder="내용입력"/>
+          </p>
+          <p>
+            <input type="submit"/>
+          </p>
         </form>
       </article>    
     );
