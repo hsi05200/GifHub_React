67fa46790d1811e5c40285b02bea27afc01bea49
stchwang <hsi05200@gmail.com>
2020-11-16 21:29:28 +0900

    추가(Create),수정(Update),삭제(Delete) 버튼을 생성하고 해당 mode를 생성한 후 추가,수정,삭제버튼 클릭시 모드를 변경하도록 작업됨

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Component } from 'react';
 import Toc from "./components/Toc";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
 import Subject from "./components/Subject";
 import Control from "./components/Control";
+import CreateContent from "./components/CreateContent";
 import './App.css';
 
 class App extends Component {
@@ -21,10 +22,11 @@ class App extends Component {
     }
   }
   render() {
-    var _title, _desc = null;
+    var _title, _desc, _content = null;
     if (this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _content = <ReadContent title={_title} desc={_desc}></ReadContent>;
     } else if (this.state.mode === 'read') {
       var i = 0;
       while (i < this.state.contents.length) {
@@ -36,6 +38,9 @@ class App extends Component {
         }
         i = i + 1;
       }
+      _content = <ReadContent title={_title} desc={_desc}></ReadContent>;
+    } else if(this.state.mode === 'create') {
+      _content = <CreateContent title={_title} desc={_desc}></CreateContent>;
     }
     return (
       <div className="App">
@@ -63,7 +68,7 @@ class App extends Component {
             mode:_mode
           });
         }.bind(this)}></Control>
-        <Content title={_title} desc={_desc}></Content>
+        {_content}
       </div>
     );
   }
---
+++ src/components/CreateContent.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+class CreateContent extends Component {  
+  render() {    
+    return (
+      <article>
+        <h2>추가</h2>
+        <form>
+      
+        </form>
+      </article>    
+    );
+  }
+}
+
+export default CreateContent;
